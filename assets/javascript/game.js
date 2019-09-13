537d5a8c398b9aa102ab9dd2f493420bf6b8bcdf
chbbs231 <atrobles231@gmail.com>
2019-09-12 18:19:10 -0700

    some idea

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,30 @@
+const characters=[{
+    name:`April`,
+    hp: 100},{
+    name:`Ron`,
+    hp: 100}, {
+    name:`Michael`,
+    hp: 100},{
+    name:`Dwight`,
+    hp: 100}]
 
-let attack = math.floor(Math.random()* 20) + 1
-let Wins = 0
-let losses = 0
+let fighter = {},
+    opponent = {},
+    fighterChosen =!1,
+    opponentChosen = !1, 
+    wins = 0;
+    
+const attack = () =>{
+    let attackButton = document.getElementById('attack-button');
+    let gameMessage = document.getElementById('game-message');
+   
+    let attack = Math.floor(Math.random()*30);
+    opponent.hp -= fighter.hp;
+    
+    gameMessage.innerText = `Opponent has attacked`
+
+    setTimeout
+    
+}
+    
+
